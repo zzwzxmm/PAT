b90622ce6c0ff8a4a75d7628dc623fe0bc3a6f20
Minmin <mmxue@nuaa.edu.cn>
2019-11-25 16:25:29 +0800

    Create 1101.cpp

---
+++ Advanced/1101.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include <algorithm>
+#include <vector>
+// copied from liu's blog;
+// NOTE: 1.position not changed elements; 2.use a Variable max to select elements that are valid in the middle.
+int v[100000];
+using namespace std;
+int main() {
+    int n, max = 0, cnt = 0;
+    scanf("%d", &n);
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        b[i] = a[i];
+    }
+    sort(a.begin(), a.end());
+    for (int i = 0; i < n; i++) {
+        if(a[i] == b[i] && b[i] > max)
+            v[cnt++] = b[i];
+        if (b[i] > max)
+            max = b[i];
+    }
+    printf("%d\n", cnt);
+    for(int i = 0; i < cnt; i++) {
+        if (i != 0) printf(" ");
+        printf("%d", v[i]);
+    }
+    printf("\n");
+    return 0;
+}
